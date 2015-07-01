dbb71f9d69cc827f700dbc6b694b0d36504c0313
Vincent Robert <vincent.robert@nanego.com>
2015-07-01 12:26:56 +0200

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_per_tracker_categories do
+  name 'Redmine Per Tracker Issue Categories plugin'
+  author 'Vincent ROBERT'
+  description 'This is a plugin for Redmine'
+  version '1.0.0'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
