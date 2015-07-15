5d889639631e762a94618cd8d703358969c6a7ec
Vincent Robert <vincent.robert@nanego.com>
2015-07-15 17:29:01 +0200

    Quick fix to make this plugin compatible with moved fields

--- app/overrides/issues/_attributes.rb
+++ app/overrides/issues/_attributes.rb
@@ -4,3 +4,12 @@ Deface::Override.new :virtual_path => 'issues/_attributes',
                      :text         => <<STRING
 <%= f.select :category_id, (@issue.project.issue_categories.reject{|c| c.tracker.present? && c.tracker != @issue.tracker }.collect {|c| [c.name, c.id]}), :include_blank => true, :required => @issue.required_attribute?('category_id') %>
 STRING
+
+# Only useful if the 'category' field has been moved to the _form partial by another plugin
+Deface::Override.new :virtual_path => 'issues/_form',
+                     :name         => 'replace-categories-according-to-tracker-in-form-partial',
+                     :replace      => 'erb[loud]:contains(":category_id")',
+                     :text         => <<STRING
+<%= f.select :category_id, (@issue.project.issue_categories.reject{|c| c.tracker.present? && c.tracker != @issue.tracker }.collect {|c| [c.name, c.id]}), :include_blank => true, :required => @issue.required_attribute?('category_id') %>
+STRING
+
