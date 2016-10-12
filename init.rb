40e5b6e3c3201e459cb5007c171ebe705b221dd6
Vincent Robert <vincent.robert@nanego.com>
2016-10-12 14:55:45 +0200

    Update general informations in init file

--- init.rb
+++ init.rb
@@ -8,8 +8,8 @@ end
 Redmine::Plugin.register :redmine_per_tracker_categories do
   name 'Redmine Per Tracker Issue Categories plugin'
   author 'Vincent ROBERT'
-  description 'This is a plugin for Redmine'
+  description 'This plugin lets you link categories and trackers. It makes them available only when the specific tracker is selected.'
   version '1.0.0'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/nanego/redmine_per_tracker_categories'
+  author_url 'mailto:contact@vincent-robert.com'
 end
