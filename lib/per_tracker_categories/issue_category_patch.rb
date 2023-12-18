042d142d13510b7c72a787ecb0e342348c769731
Vincent Robert <vincent.robert@nanego.com>
2023-12-18 16:38:31 +0100

    Enhance compatibility with Redmine 5

--- lib/per_tracker_categories/issue_category_patch.rb
+++ lib/per_tracker_categories/issue_category_patch.rb
@@ -1,16 +1,11 @@
 module PerTrackerCategories::IssueCategoryPatch
 
-  def self.included(base)
-    base.class_eval do
-      has_and_belongs_to_many :trackers
-    end
-  end
-
 end
 
 class IssueCategory
-  include Redmine::SafeAttributes
   include PerTrackerCategories::IssueCategoryPatch
+  include Redmine::SafeAttributes
   safe_attributes :tracker_ids, :position
   acts_as_positioned :scope => :project_id
+  has_and_belongs_to_many :trackers
 end
