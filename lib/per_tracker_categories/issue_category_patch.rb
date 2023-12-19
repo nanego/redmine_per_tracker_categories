26549598f40d69c0ade127f5c7ffbafc6d2e5757
Vincent <vincent.robert@nanego.com>
2023-12-19 11:08:36 +0100

    Fix core tests for Redmine 4.2

--- lib/per_tracker_categories/issue_category_patch.rb
+++ lib/per_tracker_categories/issue_category_patch.rb
@@ -1,11 +1,18 @@
+require 'issue_category'
+
 module PerTrackerCategories::IssueCategoryPatch
+
+  def self.included(base)
+    base.class_eval do
+      has_and_belongs_to_many :trackers
+    end
+  end
 
 end
 
-class IssueCategory
+class IssueCategory < ActiveRecord::Base
   include PerTrackerCategories::IssueCategoryPatch
   include Redmine::SafeAttributes
   safe_attributes :tracker_ids, :position
   acts_as_positioned :scope => :project_id
-  has_and_belongs_to_many :trackers
 end
