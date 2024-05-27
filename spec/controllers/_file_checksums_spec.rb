4caa4e33433128e07c0d6e00bdd5b736d573f12a
Vincent Robert <vincent.robert@nanego.com>
2024-05-27 13:48:24 +0200

    Update specs checksums to accomodate changes in Redmine 5.1.2

--- spec/controllers/_file_checksums_spec.rb
+++ spec/controllers/_file_checksums_spec.rb
@@ -12,7 +12,8 @@ describe "FileChecksums" do
     assert_checksum %w"7d61d99dcae956a5cd0d896dd20049b4 3e8ca31fbb2aa66bdd3ad7db808d4631", "app/views/issue_categories/create.js.erb"
 
     #the update action is completely overridden and should be adapted if anything change in Redmine core
-    assert_checksum %w"bf3ee442a603c5f0759b3fc948a6ff95 27f849c985a952b4302ac4a5acd7c1ab b5d6e3adafc967c5fdf669e5cff0375f bfcd39e39a00f922f1633ad2061304c7", "app/controllers/issue_categories_controller.rb"
+    # Redmine 5.1.2 is fine and previous ones too
+    assert_checksum %w"15dcb293c42721a34530d89570257106 bf3ee442a603c5f0759b3fc948a6ff95 27f849c985a952b4302ac4a5acd7c1ab b5d6e3adafc967c5fdf669e5cff0375f bfcd39e39a00f922f1633ad2061304c7", "app/controllers/issue_categories_controller.rb"
 
     #the issue_categories tab is completely overridden and should be adapted if anything change in Redmine core
     assert_checksum %w"4f107afaf16c1787f6546275373ccff9", "app/views/projects/settings/_issue_categories.html.erb"
