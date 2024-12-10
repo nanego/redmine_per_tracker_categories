4866c632224822cf6061c3e5e8a818a687254392
Vincent <contact@vincent-robert.com>
2024-12-10 13:36:01 +0100

    Add missing specs to ensure reorder works correctly

--- spec/controllers/issue_categories_controller_spec.rb
+++ spec/controllers/issue_categories_controller_spec.rb
@@ -9,9 +9,18 @@ describe IssueCategoriesController, :type => :controller do
   end
 
   it "should update category with tracker" do
-    put :update, params: {:id => 4, :issue_category => {:name => 'Printing', :tracker_ids => [1, 3]}}
+    put :update, params: { :id => 4, :issue_category => { :name => 'Printing', :tracker_ids => [1, 3] } }
     expect(response).to redirect_to(settings_project_path(id: 'onlinestore', tab: :categories))
     expect(IssueCategory.find(4).trackers).to eq [Tracker.find(1), Tracker.find(3)]
+  end
+
+  it "allows to reorder categories" do
+    expect(IssueCategory.find(4).position).to eq 1
+
+    put :update, params: { "id" => "4", "issue_category" => { "position" => "2" } }
+
+    expect(response).to redirect_to(settings_project_path(id: 'onlinestore', tab: :categories))
+    expect(IssueCategory.find(4).position).to eq 2
   end
 
 end
