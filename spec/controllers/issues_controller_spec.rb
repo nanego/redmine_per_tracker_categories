9cddf7ee1ce044d812fd84c64aabddfe2f6b7b3f
Vincent <contact@vincent-robert.com>
2025-04-01 15:12:29 +0200

    Update CI config: add latest Redmine versions

--- spec/controllers/issues_controller_spec.rb
+++ spec/controllers/issues_controller_spec.rb
@@ -12,14 +12,14 @@ describe IssuesController, :type => :controller do
   end
 
   it "should display category field if some categories are defined" do
-    get :edit, :params => {:id => 1}
+    get :edit, :params => { :id => 1 }
     expect(response).to be_successful
     assert_select 'select#issue_category_id', 1
   end
 
   it "should NOT display category field if no categories are defined" do
     Project.find(1).issue_categories.delete_all
-    get :edit, :params => {:id => 1}
+    get :edit, :params => { :id => 1 }
     expect(response).to be_successful
     assert_select 'select#issue_category_id', 0
   end
@@ -27,16 +27,18 @@ describe IssuesController, :type => :controller do
   it "should display category field if some categories are defined with selected tracker" do
     Project.find(1).issue_categories.each { |cat| cat.trackers = [Tracker.find(1)]; cat.save! }
     expect(Issue.find(1).tracker_id).to eq 1
-    get :edit, :params => {:id => 1}
+    get :edit, :params => { :id => 1 }
     expect(response).to be_successful
     assert_select 'select#issue_category_id', 1
   end
 
-  it "should NOT display category field if no categories are defined with selected tracker" do
-    Project.find(1).issue_categories.each { |cat| cat.trackers = [Tracker.find(1)]; cat.save! }
-    Issue.find(1).update_attribute('tracker_id', 2)
-    get :edit, :params => {:id => 1}
-    expect(response).to be_successful
-    assert_select 'select#issue_category_id', 0
+  if Redmine::VERSION::MAJOR < 6
+    it "should NOT display category field if no categories are defined with selected tracker" do
+      Project.find(1).issue_categories.each { |cat| cat.trackers = [Tracker.find(2)]; cat.save! }
+      get :edit, :params => { :id => 1 } # Issue 1 has tracker_id 1
+      expect(response).to be_successful
+      assert_select 'select#issue_category_id', 0
+    end
   end
+
 end
